ca8e5e9e90900d4aab875a8d4a5d9b1d87f7be89
Yutong Pan <yutonp@uw.edu>
2015-11-02 14:17:17 -0800

    fda

--- mad-libs/js/main.js
+++ mad-libs/js/main.js
@@ -1,8 +1,10 @@
 // Create application
-
+var myApp = angular.module('madApp', []);
 
 // Bind controller madCtrl
-
-
+myApp.controller("madCtrl", function($scope, $http){
+    $scope.name = "Angel";
 	// Set initial values for name, age, location, and food
 
+
+})
